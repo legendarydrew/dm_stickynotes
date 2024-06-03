a1c5f9ca93e09200bbb0eab55a203fd5b362f8ad
Drew Maughan <drew.maughan@toptal.com>
2024-06-03 16:01:07 +0100

    Ability to update sticky notes.

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import StickyNote 
 from .forms import NoteForm
 
@@ -22,19 +22,28 @@ def create_view(request):
     if request.method == 'POST':
         form = NoteForm(request.POST)
         if form.is_valid():
-            note = form.save(commit=False)
-            # TODO add a reference to the authenticated user.
-            note.save()
+            note = form.save()
+            # note = form.save(commit=False)
+            # note.save()
             return redirect('home')
     else:
         form = NoteForm()    
     return render(request, 'create_note.html', { 'form': form })
 
-def update_view(request):
+
+def update_view(request, note_id):
     """
     Used when the user updates a note.
     """
-    return redirect('home')
+    note = get_object_or_404(StickyNote, pk=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            note = form.save()
+            return redirect('home')
+    else:
+        form = NoteForm(instance=note)
+    return render(request, 'update_note.html', {'form': form})
 
 
 def delete_view(request):
